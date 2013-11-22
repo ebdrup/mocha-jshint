67af62babc38cf300cfeb284235eb448d8935e6b
Allan Ebdrup <aeb@e-conomic.com>
2013-11-22 10:26:14 +0100

    support multiple folders correctly.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-var jsHint = require("./node_modules/jshint/src/cli.js");
+var path = require('path');
 module.exports = function (paths) {
-	describe("JSHint", function () {
+	describe('JSHint', function () {
 		paths = paths || ['.'];
 		paths.forEach(function (p) {
-			it("should pass for " + (p === '.' ? 'working directory' : p), function () {
+			it('should pass for ' + (p === '.' ? 'working directory' : p), function () {
+				this.timeout(30000);
+				var cwd = process.cwd();
+				process.chdir(path.resolve(p));
+				delete require.cache[require.resolve('./node_modules/jshint/src/cli.js')];
+				var jsHint = require('./node_modules/jshint/src/cli.js');
 				var error = new Error('');
 				error.message = '';
 				error.stack = '';
 				var options = {
-					args: [p],
+					args: ['.'],
 					reporter: require('./reporter.js')(error)
 				};
 				jsHint.run(options);
+				process.chdir(cwd);
 				if (error.message) {
 					throw error;
 				}
--- reporter.js
+++ reporter.js
@@ -8,7 +8,7 @@ module.exports = function (err) {
 				err.message += '\n';
 				err.stack += '\n';
 			}
-			err.message += "JSHint error: " + error.reason + ' ' + file + ':' + error.line + ':' + error.character;
+			err.message += 'JSHint error: ' + error.reason + ' ' + file + ':' + error.line + ':' + error.character;
 			err.stack += error.reason + '\n at (' + path.resolve(file) + ':' + error.line + ':' + error.character + ')';
 		});
 	};
