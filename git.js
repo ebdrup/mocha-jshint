5654b5e81183eeb04011fa1bf6c3a2e6fd086ea5
Allan Ebdrup <aeb@e-conomic.com>
2015-05-05 13:55:39 +0200

    Ignore files in .jshintignore in git configuration. Fixes #13

--- git.js
+++ git.js
@@ -3,6 +3,8 @@ var format = require('util').format;
 var uniq = require('uniq');
 var path = require('path');
 var fs = require('fs');
+var shjs = require("shelljs");
+var minimatch = require("minimatch");
 
 module.exports = function (options, cb) {
 	if (!options.modified && !options.commits) {
@@ -55,13 +57,46 @@ module.exports = function (options, cb) {
 		if (err) {
 			return cb(err);
 		}
+		var jsHintIgnores = [];
+		try {
+			jsHintIgnores = fs.readFileSync(path.resolve('.jshintignore'), 'utf-8')
+				.split('\n')
+				.filter(function (line) {
+					return !!line.trim();
+				})
+				.map(function (line) {
+					if (line[0] === "!") {
+						return "!" + path.resolve(line.substr(1).trim());
+					}
+					return path.resolve(line.trim());
+				});
+		} catch (ex) {
+		}
 		var files = uniq(stdout.split('\n'))
 			.filter(function (filename) {
 				return filename.split('.').pop() === 'js';
 			})
 			.filter(function (filename) {
+				return !isIgnored(filename, jsHintIgnores);
+			})
+			.filter(function (filename) {
 				return fs.existsSync(path.resolve(filename));
 			});
 		return cb(null, files);
+
+		function isIgnored(fp, patterns) {
+			return patterns.some(function (ip) {
+				if (minimatch(path.resolve(fp), ip, {nocase: true})) {
+					return true;
+				}
+				if (path.resolve(fp) === ip) {
+					return true;
+				}
+				if (shjs.test("-d", fp) && ip.match(/^[^\/]*\/?$/) &&
+					fp.match(new RegExp("^" + ip + ".*"))) {
+					return true;
+				}
+			});
+		}
 	}
 };
